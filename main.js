4c86e72d7c697876e31436b5f5509f0cf2eb3b73
R4W1R0 <xmercado656@gmail.com>
2022-11-13 06:51:51 -0300

    Borre la barra de menu
    Ahora no se muestra en pantalla la barra superior

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const { app, BrowserWindow } = require('electron')
-const path = require('path')
 
 const createWindow = ()=>{
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        autoHideMenuBar: true,
         icon: __dirname + "/icon.ico",
     })
 
